9b183386141acbfde84691f716d12b855f9a4fa9
Bruno Fauth <149593@upf.br>
2024-04-27 16:04:45 -0300

    added interface features and fixed bugs

--- src/wifi_menu/__init__.py
+++ src/wifi_menu/__init__.py
@@ -4,8 +4,11 @@ import argparse as ap
 import asyncio
 import logging as _logging
 import operator as op
+import os
 import subprocess as sp
+import shlex
 import sys
+import textwrap
 from contextlib import suppress
 from enum import IntEnum, unique
 from functools import cache
@@ -13,7 +16,7 @@ from itertools import starmap, tee
 from typing import TYPE_CHECKING, cast
 
 import sdbus
-from fzf_but_typed import fzf_pairs, SearchOptions, ExitStatusCode as FzfExitStatus
+from fzf_but_typed import fzf_iter, SearchOptions, LayoutOptions, LayoutType, InterfaceOptions, Binding, Key, ActionSimple, ActionWithArg, ActionWithArgType, ExitStatusCode as FzfExitStatus
 from sdbus_async.networkmanager import (NetworkManager, DeviceType, NetworkDeviceGeneric,
                                         NetworkDeviceWireless, NetworkManagerSettings,
                                         NetworkConnectionSettings, AccessPoint, ActiveConnection,
@@ -54,6 +57,47 @@ if TYPE_CHECKING:
 
         def __str__(self) -> str:
             ...
+
+
+APP_NAME = os.path.basename(sys.argv[0])
+APP_NAME_QUOTED = shlex.quote(APP_NAME)
+APP_DISPLAY_NAME = "Wifi Menu"
+APP_DISPLAY_NAME_QUOTED = shlex.quote(APP_DISPLAY_NAME)
+FZF_HEADER_PARTS: list[str] = []
+
+# yapf: disable
+FZF_HEADER_PARTS.append("ctrl-d: delete profile")
+FZF_CMD_DELETE_PROFILE = textwrap.dedent("""\
+    if ! nmcli --fields="" connection show id {{}}; then
+        notify-send \\
+            --app-name={app_name} \\
+            --icon=network-wireless-disconnected \\
+            {app_display_name} \\
+            "No profile found for {{}}"
+            exit 0
+    fi
+    if nmcli connection delete {{}} >&2; then
+        notify-send \\
+            --app-name={app_name} \\
+            --icon=network-wireless-disconnected \\
+            {app_display_name} \\
+            "Deleted profile for {{}}"
+        exit 0
+    fi
+    notify-send \\
+        --app-name={app_name} \\
+        --icon=network-wireless-disconnected \\
+        {app_display_name} \\
+        "Couldn't delete profile for {{}}"
+    exit 1
+""")
+
+FZF_HEADER_PARTS.append("ctrl-r: rescan wifi access points")
+FZF_CMD_RESCAN = textwrap.dedent("""\
+    nmcli --terse --fields=SSID device wifi list --rescan yes
+""")
+
+# yapf: enable
 
 
 @unique
@@ -124,20 +168,24 @@ async def get_access_points(dev: NetworkDeviceWireless) -> dict[str, AccessPoint
     return {path: AccessPoint(path) for path in access_points}
 
 
-async def _get_ssid(ap: AccessPoint) -> str:
-    return cast(bytes, await ap.ssid).decode('utf-8', errors="replace")
+async def _get_ssid_ap_pair(ap: AccessPoint) -> tuple[str, AccessPoint]:
+    return cast(bytes, await ap.ssid).decode('utf-8', errors="replace"), ap
 
 
 def get_args() -> dict[str, Any]:
     parser = ap.ArgumentParser()
-    parser.add_argument('-v', '--verbose', action="count", default=0)
+    parser.add_argument('-v', '--verbose', action="count", default=0, help="Can be used up to 3 times")
     parser.add_argument('-s', '--scan', action="store_true", default=False)
     return vars(parser.parse_args())
 
 
-def fzf_pairs_wrapper(data: Iterable[tuple[SupportsStr, SupportsStr]], **kwargs) -> list[str]:
+def fzf_wrapper(data: Iterable[SupportsStr], **kwargs) -> list[str]:
     try:
-        return fzf_pairs(data, search=SearchOptions(exact=True))
+        if (so := kwargs.get('search', None)) is not None:
+            so.exact = True
+        else:
+            kwargs['search'] = SearchOptions(exact=False)
+        return fzf_iter(data, **kwargs)
     except sp.CalledProcessError as error:
         match error.returncode:
             case FzfExitStatus.NO_MATCH:
@@ -154,6 +202,7 @@ def fzf_pairs_wrapper(data: Iterable[tuple[SupportsStr, SupportsStr]], **kwargs)
 
 
 def new_profile(ssid: str, interface_id: str) -> ConnectionProfile:
+    from getpass import getpass
     return ConnectionProfile(
         connection=ConnectionSettings(
             autoconnect=True,
@@ -162,7 +211,7 @@ def new_profile(ssid: str, interface_id: str) -> ConnectionProfile:
             interface_name=interface_id,
         ),
         wireless=WirelessSettings(ssid=ssid.encode("utf-8")),
-        wireless_security=WirelessSecuritySettings(key_mgmt="wpa-psk", psk=input("Password: ")),
+        wireless_security=WirelessSecuritySettings(key_mgmt="wpa-psk", psk=getpass()),
     )
 
 
@@ -191,13 +240,42 @@ async def main(verbose: int = 0, scan: bool = False) -> int:
         logger.info("Device on interface '%s' hasn't detected any access points. Exiting.", wifi_interface)
         return ExitStatus.NO_ACCESS_POINT
     access_points = [AccessPoint(path) for path in cast(list[str], ap_paths)]
-    ssids: list[str] = await asyncio.gather(*map(_get_ssid, access_points))
+    ap_by_ssid: dict[str, AccessPoint] = \
+        dict(await asyncio.gather(*map(_get_ssid_ap_pair, access_points)))
+    ssids = list(ap_by_ssid.keys())
 
-    result = fzf_pairs_wrapper(enumerate(ssids))
-    chosen_ap_index = int(result[0])
-    chosen_ap_path = ap_paths[chosen_ap_index]
-    chosen_ap_ssid = ssids[chosen_ap_index]
-    logger.debug("Using access point: %s", chosen_ap_ssid)
+    cmd_delete_profile = FZF_CMD_DELETE_PROFILE.format(
+        app_name=APP_NAME_QUOTED,
+        app_display_name=APP_DISPLAY_NAME_QUOTED,
+    )
+    logger.debug(cmd_delete_profile)
+    # yapf: disable
+    result = fzf_wrapper(ap_by_ssid.keys(),
+        layout=LayoutOptions(
+            layout=LayoutType.REVERSE_LIST,
+            header="\n".join(FZF_HEADER_PARTS),
+            header_first=True,
+        ),
+        interface=InterfaceOptions(bind=[
+            Binding(binding=Key.CTRL_D, actions=[
+                ActionWithArg(
+                    action_type=ActionWithArgType.EXECUTE,
+                    argument=cmd_delete_profile),
+            ]),
+            Binding(binding=Key.CTRL_R, actions=[
+                ActionWithArg(
+                    action_type=ActionWithArgType.RELOAD,
+                    argument=FZF_CMD_RESCAN)
+            ]),
+            Binding(binding=Key.ESC, actions=[ActionSimple.CANCEL]),
+        ])
+    )
+    chosen_ap_ssid = result[0]
+    if chosen_ap_ssid not in ssids:
+        raise RuntimeError(f"Chose a newly discovered network, of ssid: {chosen_ap_ssid!r}")
+    chosen_ap_path = cast(str, ap_by_ssid[chosen_ap_ssid]._remote_object_path)
+    logger.debug("Using access point: '%s' (%s)", chosen_ap_ssid, chosen_ap_path)
+    # yapf: enable
 
     ncs_paths: list[str] = await get_nm_settings().connections
     network_conn_settings = list(map(NetworkConnectionSettings, ncs_paths))
@@ -240,11 +318,11 @@ async def main(verbose: int = 0, scan: bool = False) -> int:
             try:
                 notification_server = FreedesktopNotifications(bus=sdbus.sd_bus_open_user())
                 _notification_id = await notification_server.notify(
-                    app_name=sys.argv[0],
+                    app_name=APP_NAME,
                     app_icon='network-wireless-connected',
                     summary='Wi-Fi connection established',
                     body=f'connected to {chosen_ap_ssid!r}',
-                    expire_timeout=-1,
+                    hints=notification_server.create_hints(urgency=1),
                 )
             except NameError:
                 pass
@@ -259,11 +337,12 @@ async def main(verbose: int = 0, scan: bool = False) -> int:
             try:
                 notification_server = FreedesktopNotifications(bus=sdbus.sd_bus_open_user())
                 _notification_id = await notification_server.notify(
-                    app_name=sys.argv[0],
+                    app_name=APP_NAME,
                     app_icon='network-wireless-offline',
-                    summary="Couldn't establish Wi-Fi connection",
-                    body=f"couldn't connect to {chosen_ap_ssid!r}",
-                    expire_timeout=-1,
+                    summary=f"Couldn't connect to {chosen_ap_ssid!r}",
+                    body=f"maybe the network password changed?",
+                    expire_timeout=0,
+                    hints=notification_server.create_hints(urgency=2),
                 )
             except NameError:
                 pass
